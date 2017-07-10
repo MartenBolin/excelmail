f0af927a034f3bbf1683d4b5c803e5d645792616
Mårten Bolin <Mrten Bolin>
2017-07-10 19:36:20 +0200

    mhm

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
 from os import environ
 
-app.run(host= '0.0.0.0', debug=True, port=environ.get("PORT", 5000), threaded = True)
+port = int(environ.get('PORT', 5000))
+app.run(host= '0.0.0.0', debug=True, port=port, threaded = True)
